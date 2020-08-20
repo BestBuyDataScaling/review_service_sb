ed88cad615d512c9b6923fecfb3d0f6521d088c6
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-08-20 11:19:10 -0500

    Runs production setup for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'review_service_rw',
+    script: './server/index.js',
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-218-79-61.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/FEC-BestBuy-RW.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/ATX-50-Team-Best-Buy/review_service_rw.git',
+      path: '/home/ubuntu/review-service',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
+};
